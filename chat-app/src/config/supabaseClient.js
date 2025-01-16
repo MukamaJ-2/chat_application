09c75187c280cb8423b39ef5a4a8068fabfbb126
MukamaJ-2 <145841039+MukamaJ-2@users.noreply.github.com>
2025-01-16 09:37:06 +0300

    n

--- chat-app/src/config/supabaseClient.js
+++ chat-app/src/config/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://yxrvcyvqdkhjojchhnrs.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4cnZjeXZxZGtoam9qY2hobnJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NDMzMzUsImV4cCI6MjA1MjUxOTMzNX0.pOO7xR9gjUlU9bmT2b_T6yFG-xmWmSkuU5P9arvNd8Y";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
